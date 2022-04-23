98f569047a956b6e438b74a0e005719d9419c6f5
mauriciomaca <mauricio.maca@gmail.com>
2022-04-23 14:44:31 -0500

    main modificado

---
+++ main_modi.py
@@ -0,0 +1,21 @@
+# Curso: Introducción a la programación.
+# Lenguaje: Python. Por: Mauricio Maca Chagüendo
+# Temas: Introducción a un IDE. Creación de un proyecto.
+#        Explicación, ejecución y modificación del script main.py de PyCharm.
+
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def imprimir_saludo(nombre):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hola, {nombre}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    imprimir_saludo('Mauricio')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
